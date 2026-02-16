26433360f8f7dfa2b8c71f5b5922a02ac63f000e
Captain Joni <captain.joni@thepic.de>
2026-02-16 16:11:55 +0100

    create

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 
 
 // Public Marker-Seite ausliefern
-app.use('/public', express.static(path.join(__dirname, 'frontend', 'public')));
+app.use('/create', express.static(path.join(__dirname, 'frontend', 'public')));
 
 
 // Routes
